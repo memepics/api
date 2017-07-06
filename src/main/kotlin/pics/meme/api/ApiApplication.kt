9de5e6a34cd5f9e0cc208fce514fa914dc31bafb
Javi Martin <webmasterhhp@gmail.com>
2017-07-06 15:00:53 +0200

    Starting spring boot project

---
+++ src/main/kotlin/pics/meme/api/ApiApplication.kt
@@ -0,0 +1,11 @@
+package pics.meme.api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ApiApplication::class.java, *args)
+}
